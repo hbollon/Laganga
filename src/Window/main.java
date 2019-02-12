4867d618d8c8b8898f2bc56e4cb243251ec82e62
Hugo <hugo.bollon@gmail.com>
2019-02-12 13:53:51 +0100

    v0.02

--- src/Window/main.java
+++ src/Window/main.java
@@ -1,21 +1,50 @@
 package Window;
 
-import java.awt.Image;
+import java.awt.BorderLayout;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTree;
 
 public class main {
-
+	
+	static private JFrame fenetre;
+	static private JTree listeUser;
+	static private JPanel panelGauche;
+	
 	public static void main(String[] args) {
-		JFrame fenetre = new JFrame();
-		Image img = new ImageIcon("");
+		fenetre = new JFrame();
+		listeUser = new JTree();
+		initializeFrame(fenetre);
+		initializeListe(listeUser);
+		fenetre.setContentPane(getJPanel());
 
-	    fenetre.setTitle("Laganga");
-	    fenetre.setSize(1920, 1080);
+	  }       
+	
+	public static void initializeFrame(JFrame fenetre)
+	{
+		fenetre.setTitle("Laganga");
+	    fenetre.setSize(1280, 720);
 	    fenetre.setLocationRelativeTo(null);
+	    ImageIcon img = new ImageIcon("icon.png");
+	    fenetre.setIconImage(img.getImage());
 	    fenetre.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    fenetre.setVisible(true);
-	  }       
+	}
+	
+	public static void initializeListe(JTree listeUser)
+	{
+		listeUser = new JTree();
+		listeUser.setSize(200, 420);
 	}
 
+	public static JPanel getJPanel()
+	{
+		listeUser.setSize(200, 420);
+		panelGauche = new JPanel();
+		panelGauche.setLayout(new BorderLayout());
+		panelGauche.add(
+		return panelGauche;
+	}
 }
