8be759161a846f2d346c997eb08e7a0d64c26f61
Julien Valverdé <julien.valverde@netc.fr>
2019-04-14 01:16:39 +0200

    Modification du splash

--- src/view/SplashWin.java
+++ src/view/SplashWin.java
@@ -4,7 +4,6 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
